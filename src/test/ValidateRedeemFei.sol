7afd846e7c36a4e5104a91b970c3724b99624589
defijesus.eth <defijesus@buzzedbearhideout.com>
2022-09-27 17:14:03 +0100

    fix tests and add validation for liquidity after redeem.

--- src/test/ValidateRedeemFei.sol
+++ src/test/ValidateRedeemFei.sol
@@ -50,11 +50,11 @@ contract ValidationRedeemFei is Test {
     }
 
     function testProposalPostPayload() public {
-        uint256 aFeiPoolBalance = IERC20(FEI).balanceOf(AAVE_LENDING_POOL);
+        uint256 aFeiBalance = IERC20(FEI).balanceOf(A_FEI);
         uint256 aDaiReserveBalanceBefore = IERC20(A_DAI).balanceOf(AAVE_MAINNET_RESERVE_FACTOR);
         uint256 aFeiReserveBalanceBefore = IERC20(A_FEI).balanceOf(AAVE_MAINNET_RESERVE_FACTOR);
-        uint256 minBalance = aFeiPoolBalance - (aFeiPoolBalance * 3 / 10_000);
-        uint256 psmAmountOut = DAI_FIXED_PRICE_PSM.getRedeemAmountOut(aFeiPoolBalance);
+        uint256 minBalance = aFeiBalance - (aFeiBalance * 3 / 10_000);
+        uint256 psmAmountOut = DAI_FIXED_PRICE_PSM.getRedeemAmountOut(aFeiBalance);
 
         address[] memory targets = new address[](1);
         targets[0] = address(feiPayload);
@@ -84,7 +84,7 @@ contract ValidationRedeemFei is Test {
         AaveGovHelpers._passVote(vm, AAVE_WHALE, proposalId);
         uint256 aDaiReserveBalanceAfter = IERC20(A_DAI).balanceOf(AAVE_MAINNET_RESERVE_FACTOR);
         uint256 aFeiReserveBalanceAfter = IERC20(A_FEI).balanceOf(AAVE_MAINNET_RESERVE_FACTOR);
-        emit log_named_uint("Amount of FEI in LendingPool", aFeiPoolBalance);
+        emit log_named_uint("Amount of FEI in LendingPool", aFeiBalance);
         emit log_named_uint("Max DAI Redeem from PSM after 3bps fee -----------", minBalance);
         emit log_named_uint("PSM expected output amount from FEI in LendingPool", psmAmountOut);
         emit log_named_uint("aDAI balance before ----------------", aDaiReserveBalanceBefore);
@@ -98,6 +98,8 @@ contract ValidationRedeemFei is Test {
         assertGe(aDaiReserveBalanceAfter, aDaiReserveBalanceBefore + minBalance);
 
         // Compensating for +1/-1 precision issues when rounding, mainly on aTokens
-        assertApproxEqAbs(aFeiReserveBalanceAfter, aFeiReserveBalanceBefore - aFeiPoolBalance, 1);
+        assertApproxEqAbs(aFeiReserveBalanceAfter, aFeiReserveBalanceBefore - aFeiBalance, 1);
+
+        assertEq(IERC20(FEI).balanceOf(A_FEI), 0);
     }
 }
